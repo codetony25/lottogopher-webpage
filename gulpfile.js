06912d577a75e6710db6b0ac53aca1a76a0e8deb
Tony Nguyen <codetony25@gmail.com>
2017-04-28 13:50:42 -0700

    fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,15 @@ var gulp = require('gulp');
 var scssPlugin = require('gulp-sass');
 var connect = require('gulp-connect');
 var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
 
 var autoprefixerOptions = {
   browsers: ['last 4 versions', '> 5%', 'Firefox ESR']
 };
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-  browserSync.init({
-    server: "./app"
-  });
-
-  gulp.watch('css/*.scss', ['myStyles']);
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
-
 gulp.task('myStyles', function () {
   gulp.src('css/*.scss')
-    .pipe(scssPlugin())
+    .pipe(scssPlugin({ outputStyle: 'expanded' }))
+    .pipe(autoprefixer(autoprefixerOptions))
     .pipe(gulp.dest('css'))
     .pipe(connect.reload());
 });
@@ -44,7 +33,7 @@ gulp.task('watchMyStyles', function() {
   gulp.watch('css/*.scss', ['myStyles']);
 });
 
-// gulp.task('default', ['watchMyStyles', 'connect']);
+gulp.task('default', ['watchMyStyles', 'connect']);
 
 // uncomment to build prod version of css
-gulp.task('default', ['prod']);
+// gulp.task('default', ['prod']);
