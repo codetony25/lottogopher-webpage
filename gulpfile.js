6ab9ec2a5e86a190633e1c0f81983648fc8d74e8
Tony Nguyen <codetony25@gmail.com>
2017-04-28 16:28:10 -0700

    remove active tag on index and moved arrows down a bit for mobile

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,17 @@ gulp.task('sass', function () {
     .pipe(bs.reload({stream: true}));
 });
 
+gulp.task('prod', function () {
+  return gulp
+    .src('css/*.scss')
+    .pipe(scssPlugin({ outputStyle: 'expanded' }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['prod']);
+
+
 gulp.task('watch', ['browser-sync'], function () {
   gulp.watch("css/*.scss", ['sass']);
   gulp.watch("*.html").on('change', bs.reload);
