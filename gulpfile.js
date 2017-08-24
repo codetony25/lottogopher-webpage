affbeac84e42be4fb1fe4be5eb095d8cec9fdadd
Tony Nguyen <codetony25@gmail.com>
2017-08-24 14:46:03 -0700

    added landing page to prod

--- gulpfile.js
+++ gulpfile.js
@@ -23,19 +23,19 @@ gulp.task('sass', function () {
 });
 
 
-// gulp.task('prod', function () {
-//   return gulp
-//     .src('css/*.scss')
-//     .pipe(sass({ outputStyle: 'expanded' }))
-//     .pipe(autoprefixer(autoprefixerOptions))
-//     .pipe(gulp.dest('./dist/css'));
-// });
-//
-// gulp.task('default', ['prod']);
-
-gulp.task('watch', ['browser-sync'], function () {
-  gulp.watch("css/*.scss", ['sass']);
-  gulp.watch("*.html").on('change', bs.reload);
+gulp.task('prod', function () {
+  return gulp
+    .src('css/*.scss')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['prod']);
+
+// gulp.task('watch', ['browser-sync'], function () {
+//   gulp.watch("css/*.scss", ['sass']);
+//   gulp.watch("*.html").on('change', bs.reload);
+// });
+//
+// gulp.task('default', ['watch']);
